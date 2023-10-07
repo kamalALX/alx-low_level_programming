e70c8015614941738f345e404c5fbbf59abc3e18
Kamal <kamalmiftah01@gmail.com>
2023-10-06 17:44:54 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all  single digit numbers starting from zero
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar('n');
+		++n;
+	}
+	putchar('\n');
+
+	return (0);
+}
