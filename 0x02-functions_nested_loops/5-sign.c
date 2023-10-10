2809196ba7364019111922f05599ba8d154a6ea3
Kamal <kamalmiftah01@gmail.com>
2023-10-10 16:36:10 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - print + if n is greater than 0
+ *		print 0 if n equal to 0
+ *		print - if n less than 0
+ *
+ * @n: input of prinnt_sign function
+ *
+ * Return: return 1 if n greater than 0
+ *		return 0 if n equal to 0
+ *		return -1 if n less than 0
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{i
+		return (1);
+		_putchar(43);
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar(45);
+	}
+}
