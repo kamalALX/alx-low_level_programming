ec3803992eaa36641fd8c96e6f6ea00224d63e1f
Kamal <kamalmiftah01@gmail.com>
2023-10-10 06:50:46 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: use printf function
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
