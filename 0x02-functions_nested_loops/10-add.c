82806071e58bde5d38311cada8f8860206b9ac51
Kamal <kamalmiftah01@gmail.com>
2023-10-10 18:26:19 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - calculate the sum of 2 integers
+ *
+ * @i: First integer value
+ * @j: Second integer value
+ *
+ * Return: return the value of int + int
+*/
+
+int add(int i, int j)
+{
+	int r;
+
+	r = i + j;
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{
+}
