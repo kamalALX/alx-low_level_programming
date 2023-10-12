03617efd705c1baac1beba56c49fdd34cc9b036c
Kamal <kamalmiftah01@gmail.com>
2023-10-12 16:59:47 -0700

    print characters

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + 48);
+	_putchar('\n');
+}
