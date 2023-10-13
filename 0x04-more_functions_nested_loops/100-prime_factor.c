41ba27cd9482c8e70e11a8d75114bd7302f63bd9
Kamal <kamalmiftah01@gmail.com>
2023-10-13 14:19:15 -0700

    printlargest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+nclude <stdio.h>
+/**
+ * main - entry point
+ * Description: find the largest prime factor of the number 612852475143
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	long fact;
+	long num = 612852475143;
+
+	for (fact = 2; fact <= num; fact++)
+	{
+		if (num % fact == 0)
+		{
+			num /= fact;
+			fact--;
+		}
+	}
+	printf("%ld\n", fact);
+	   return (0);
+}
