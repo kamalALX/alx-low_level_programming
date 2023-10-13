e03b35eb22fa37d30463a021c7f5f71e01e0f673
Kamal <kamalmiftah01@gmail.com>
2023-10-13 11:09:01 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,32 +12,32 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
-	{
-		if (n % 3 == 0)
+		for (n = 1; n <= 100; n++)
 		{
-			printf("Fizz");
+			if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else if (n % 3 == 0 && n % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("%d", n);
+			}
+			if (n < 100)
+			{
+				printf(" ");
+			}
+			else
+			{
+				printf("\n");
+			}
 		}
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else if (n % 3 == 0 && n % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else
-		{
-			printf("%d", n);
-		}
-		if (n < 100)
-		{
-			printf(" ");
-		}
-		else
-		{
-			printf("\n");
-		}
-	}
 	return (0);
 }
