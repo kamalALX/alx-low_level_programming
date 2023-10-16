70415c2d92e52ccea46b9db5b86904aed4a1df85
Kamal <kamalmiftah01@gmail.com>
2023-10-16 11:07:09 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,15 @@
 
 void puts_half(char *str)
 {
-	int n = 0, i, length;
+	int n = 0, i;
 
 	while (str[n] != '\0')
 	{
 		n++;
 	}
-	length = n;
-	i = (length) / 2;
-
-	while (i <= length)
+	n++;
+	i = n / 2;
+	while (str[i] != '\0')
 	{
 		putchar(str[i]);
 		i++;
