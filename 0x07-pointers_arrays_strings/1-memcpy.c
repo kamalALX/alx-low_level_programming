f4910abef21cb848074abd3200b277eadb3343e1
Kamal <kamalmiftah01@gmail.com>
2023-10-23 12:37:24 -0700

    tasks 0 & 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memcpy - unction that copies memory area.
+ *
+ * @dest: input
+ * @src: input
+ * @n: input
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
