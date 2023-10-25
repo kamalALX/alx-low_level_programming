12c36068f52cd770b254e1ebea1506e5efb3c9ae
Kamal <kamalmiftah01@gmail.com>
2023-10-25 14:00:55 +0000

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -17,9 +17,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else
-	{
-		n = x * _pow_recursion (x, y - 1);
+	n = x * _pow_recursion(x, y - 1);
 		return (n);
-	}
 }
