5e508d877e2cecd63b1166e469270b7263b066fa
Kamal <kamalmiftah01@gmail.com>
2023-10-27 21:34:41 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 		printf("0\n");
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
@@ -27,7 +27,6 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-				
 		}
 		res += atoi(argv[i]);
 	}
