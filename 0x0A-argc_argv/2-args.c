7a04e819843c1506768e1487c8e679ea700a7202
Kamal <kamalmiftah01@gmail.com>
2023-10-27 19:59:10 +0000

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that prints the number of arguments passed into it.
+ *
+ * @argc: The count of the arguments supplied to the program
+ * @argv: 'array of pointers to char'.
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+        	printf("%s\n", argv[i]);
+	}
+        return (0);
+}
