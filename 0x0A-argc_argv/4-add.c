fd109d9a4a6e7fffb4f545c2805302a85fce82b1
Kamal <kamalmiftah01@gmail.com>
2023-10-27 21:31:55 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a program that adds positive numbers.
+ *
+ * @argc: The count of the arguments supplied to the program
+ * @argv: 'array of pointers to char'.
+ *
+ * Return: 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, res;
+
+	if (argc < 2)
+		printf("0\n");
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+				
+		}
+		res += atoi(argv[i]);
+	}
+	printf("%i\n", res);
+	return (0);
+}
