aa6d8d2245c9231215ea06a1bb9b3dffd3defc6d
Kamal <kamalmiftah01@gmail.com>
2023-10-30 19:38:46 +0100

    project: 0x0B-malloc_free, task: 1.

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_array - function that returns a pointer to
+ *		a newly allocated space in memory,
+ *		which contains a copy of the string given as a parameter.
+ *
+ * @str: pointer to string to be copied.
+ *
+ * Return: returns a pointer to a new string.
+ *	- returns NULL if str = NULL.
+ *	returns NULL if insufficient memory was available.
+*/
+
+char *_strdup(char *str)
+{
+	char *p;
+	char *p;
+	int l = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	p = (char*) malloc(sizeof(char) * l);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= l; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
