d23f2191fc1f999aa7c034215bf68b0d67458914
Kamal <kamalmiftah01@gmail.com>
2023-10-30 19:43:48 +0100

    project: 0x0B-malloc_free, task: 1.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -23,7 +23,7 @@ char *_strdup(char *str)
 	{
 		l++;
 	}
-	p = (char *) malloc(sizeof(char) * (l + 1);
+	p = (char *) malloc(sizeof(char) * (l + 1));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i <= l; i++)
