56862a45aa9b124bbdbba4ff38997bb6b8069aa6
Kamal <kamalmiftah01@gmail.com>
2023-10-31 01:18:29 +0100

    project: 0x0B-malloc_free, task: 4.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - a function that returns a pointer
+ *		to a 2 dimensional array of integers.
+ *
+ * @grid: pointer to array of pointers
+ * @height: input
+ *
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+}
