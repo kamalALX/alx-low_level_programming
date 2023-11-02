f0c32e977099029607ab84c0f8c94f335abb32db
Kamal <kamalmiftah01@gmail.com>
2023-11-02 12:18:56 +0100

    fixing main.h file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,13 +6,13 @@
 
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
-unsigned int my_len(char *p)
-	int _putchar(char c);
-	void *malloc_checked(unsigned int b);
-	void *_calloc(unsigned int nmemb, unsigned int size);
-	int *array_range(int min, int max);
-	void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-	int _atoi(char *s);
-	void check_int(int argc, char *argv[]);
+unsigned int my_len(char *p);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _atoi(char *s);
+void check_int(int argc, char *argv[]);
 
 #endif /*MAIN_H*/
