f77dd13678bf5c8a92fa7fa21cdc1cc9a388d49c
Kamal <kamalmiftah01@gmail.com>
2023-11-02 17:02:41 +0100

    fix for task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
  * @n :the first n bytes of s2 to use
  *
  * Return: pointer to the allocated memory wich contain s1 + s2.
-*/
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -19,13 +19,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		length = length1 + n;
 	else
 		length = length1 + length2;
-	p = malloc(sizeof(char) * (length + 1));
+	p = malloc(sizeof(char) * length + 1);
 	if (p == NULL)
 		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 ="";
+		s2 = "";
 	for (i = 0, j = 0; s1[j]; i++, j++)
 		p[i] = s1[j];
 
@@ -41,7 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  * @p: pointer the string.
  *
  * Return: the length of the string.
-*/
+ */
 
 unsigned int my_len(char *p)
 {
