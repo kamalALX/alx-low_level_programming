004ba261ad4918f86285c2f3c05ec9733903093f
Kamal <kamalmiftah01@gmail.com>
2023-11-02 16:13:47 +0100

    fix for task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,7 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	int *intptr;
 
-	intptr = (int*)ptr;
+	intptr = (int *)ptr;
 
 	if (new_size == 0 && ptr != NULL)
 	{
