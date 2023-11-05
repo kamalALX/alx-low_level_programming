8642cf275c6f3219842abd471bd9d234955ae9cd
Kamal <kamalmiftah01@gmail.com>
2023-11-05 20:46:14 +0100

    0x0E-structures_typedef. task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+/**
+ * struct dog - struct
+ *
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
