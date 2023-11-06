9fbaa059ab0f619f88c4a858017f054dab709157
Kamal <kamalmiftah01@gmail.com>
2023-11-06 01:08:53 +0100

    0x0E-structures_typedef. task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs.
+ *
+ * @d: the dog that we should set free.
+ *
+*/
+
+void free_dog(dog_t *d)
+{
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
