797b46034787c65e352a8f4e1f25b494dbd24d25
Kamal <kamalmiftah01@gmail.com>
2023-11-09 13:15:26 +0100

    project: 0x10-variadic_functions; fix task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,17 +14,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (*separator != '\0')
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(args, int));
+		printf("%d", va_arg(args, int));
 
-			if (i != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(args);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(args);
 }
 
