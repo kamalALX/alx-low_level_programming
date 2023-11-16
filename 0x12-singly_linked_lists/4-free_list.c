6aaea880fac2673b4bb1997f9a5962c380a0d29a
Kamal <kamalmiftah01@gmail.com>
2023-11-16 21:22:16 +0100

    0x12. C - Singly linked lists; task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,14 +9,13 @@
 
 void free_list(list_t *head)
 {
-	int i;
-	list_t tmp;
+	list_t *tmp;
 
-	for (i = 0; head != NULL; i++)
+	tmp = head;
+	while (tmp != NULL)
 	{
 		tmp = head->next;
-		free(head->str);
+		free(tmp->str);
 		free(head);
-		head = tmp;
 	}
 }
