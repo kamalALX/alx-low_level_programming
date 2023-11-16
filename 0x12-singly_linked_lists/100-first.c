1b275f4012041d412eeac980666cd34fa5ffc4d2
Kamal <kamalmiftah01@gmail.com>
2023-11-16 22:30:59 +0100

    0x12. C - Singly linked lists; task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - a function that prints a string
+ *		befor the main function is executed.
+*/
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
