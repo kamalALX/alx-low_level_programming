9ab337d001db3a53063e6c6275502df89af563a5
Kamal <kamalmiftah01@gmail.com>
2023-11-16 15:33:30 +0100

    adding lists.h header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,10 @@
 #include <string.h>
 #include <stdio.h>
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+
 /**
  * struct list_s - singly linked list
  *
