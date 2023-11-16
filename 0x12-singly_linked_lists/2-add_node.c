5748cf14dec37b9c8e3dc8e65221763399142721
Kamal <kamalmiftah01@gmail.com>
2023-11-16 15:31:52 +0100

    alias gc='git commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+/**
+ * add_node - a function that adds a new node at the beginning of
+ *		a list_t list.
+ * @str: the new node to add.
+ * @head: pointer to the head of the list.
+ *
+ * Return:  the address of the new element, or NULL if it failed.
+*/
+
+ list_t *add_node(list_t **head, const char *str)
+{
+	int length;
+	char *dupl;
+	list_t *newnode;
+
+	newnode =  malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	dupl = strdup(str);
+	if (dupl == NULL)
+	{
+		free(dupl);
+		return (NULL);
+	}
+
+	for (length = 0; str[length];)
+		length++;
+
+	newnode->str = dupl;
+	newnode->len = length;
+	newnode->next = *head;
+
+	*head = newnode;
+
+
+	return (newnode);
+}
