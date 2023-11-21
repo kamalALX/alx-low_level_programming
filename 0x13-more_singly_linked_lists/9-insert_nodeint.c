800b671c9178a1e328bb4b8d08e3e6187fc82824
Kamal <kamalmiftah01@gmail.com>
2023-11-21 23:59:25 +0100

    fix for task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 				{
 					newnode->n = n;
 					newnode->next = *head;
-					head = newnode;
+					*head = newnode;
 					return (newnode);
 				}
 				newnode->next = temp->next;
