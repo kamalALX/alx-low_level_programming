cc4ae9891ff89e9babb77aaed6e1e4e32abb6550
Kamal <kamalmiftah01@gmail.com>
2023-11-21 23:16:09 +0100

    fix for task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,54 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: A pointer to a singly linked list node structure.
+ * has_loop - a function that finds a loop in a linked list
+ * @head: linked list to search
  *
- * Return: The number of nodes in the list.
+ * Return: address of node where the loop starts
+ *		- returns NULL if there is no loop.
+*/
+
+listint_t *has_loop(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+			if (ptr == end->next)
+				return (end->next);
+	}
+	return (NULL);
+}
+
+/**
+ * print_listint_safe - a function that prints a linked list, even if it has a loop
+ * @head: pointer to the head of the list.
+ *
+ * Return: number of nodes in the list.
 */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current = head;
+	int loop;
+	listint_t *loopnode;
 
-	if (head == NULL)
-		exit(98);
-	while (current != NULL)
+	loopnode = has_loop((listint_t *) head);
+
+	for (count = 0, loop = 1; (head != loopnode || loop) && head != NULL; count++)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		if (current <= current->next)
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
-		current = current->next;
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (head == loopnode)
+			loop = 0;
+		head = head->next;
 	}
+	if (loopnode != NULL)
+		printf("-> [%p] %d\n", (void *) head, head->n);
 	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,5 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 unsigned int listint_length(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+listint_t *has_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 #endif /* LINKED_LISTS */
