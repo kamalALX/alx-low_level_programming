1fd27404820cd1ff8692ec4ac30aa361d57486e0
Kamal <kamalmiftah01@gmail.com>
2023-11-21 23:41:19 +0100

    tasks 12

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -32,39 +32,35 @@ listint_t *find_loop_2(listint_t *head)
  *
  * Return: number of nodes freed
 */
+
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *next, *loopnode;
-	size_t len = 0;
+	size_t len;
 	int loop = 1;
 
 	if (h == NULL || *h == NULL)
 		return (0);
 
 	loopnode = find_loop_2(*h);
-
-	while (*h != loopnode || loop)
+	for (len = 0; (*h != loopnode || loop) && *h != NULL; *h = next)
 	{
 		len++;
 		next = (*h)->next;
-		free(*h);
-
 		if (*h == loopnode && loop)
 		{
 			if (loopnode == loopnode->next)
 			{
+				free(*h);
 				break;
 			}
 			len++;
 			next = next->next;
-			free(next);
+			free((*h)->next);
 			loop = 0;
 		}
-
-		*h = next;
+		free(*h);
 	}
-
 	*h = NULL;
-	return len;
+	return (len);
 }
-
