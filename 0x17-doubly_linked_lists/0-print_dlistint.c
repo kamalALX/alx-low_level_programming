9639f4afa7ace563692c362f2adb9211a7fd1bd6
Kamal <kamalmiftah01@gmail.com>
2024-01-09 18:29:06 +0100

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 
-/***/
+/**
+ * print_dlistint - a function that prints all the elements
+ *					of a dlistint_t list.
+ *
+ * @h: a pointer to the list
+ *
+ * Return: number of noded in the list
+*/
 
 size_t print_dlistint(const dlistint_t *h)
 {
