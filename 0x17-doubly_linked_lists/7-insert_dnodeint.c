e9ccc0ffe096de73a275f8d2fe465babd846a099
Kamal <kamalmiftah01@gmail.com>
2024-01-09 21:38:14 +0100

    fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * dlistint_len1 -  function that returns the number of elements in a linked
- * @h: pointer to the linked list.
- * Return:lent of the list.
-*/
-
-unsigned int dlistint_len1(const dlistint_t *h)
-{
-	unsigned int count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
-
-
-/**
  * insert_dnodeint_at_index -  function that inserts a new node
  *					at a given position.
  * @h: pointer tothe head of the node
@@ -33,38 +11,34 @@ unsigned int dlistint_len1(const dlistint_t *h)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int insert = 0;
+	unsigned int insert = 1;
 	dlistint_t *newnode = NULL;
-	unsigned int len;
 
 	while ((*h)->prev)
 		*h = (*h)->prev;
-	len = dlistint_len1(*h);
-	if (len == idx)
+	if (idx == 0)
 	{
-		newnode = add_dnodeint_end(h, n);
+		newnode = add_dnodeint(h, n);
 		return (newnode);
 	}
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
-		return (NULL);
-
 	while (*h)
 	{
 		if (insert == idx)
 		{
-			newnode->n = n;
-			newnode->next = NULL;
-			newnode->prev = NULL;
-			if (*h != NULL)
+			if ((*h)->next == NULL)
+				newnode = add_dnodeint_end(h, n);
+			else
 			{
-				newnode->prev = (*h)->prev;
-				newnode->next = *h;
-				if (idx != 0)
-					((*h)->prev)->next = newnode;
-				(*h)->prev = newnode;
+				newnode = malloc(sizeof(dlistint_t));
+				if (newnode != NULL)
+				{
+					newnode->n = n;
+					newnode->next = (*h)->next;
+					newnode->prev = *h;
+					(*h)->next->prev = newnode;
+					(*h)->next = newnode;
+				}
 			}
-			*h = newnode;
 			return (newnode);
 		}
 		*h = (*h)->next;
