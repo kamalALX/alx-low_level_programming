0cf53da69ef4d6d85e64c856c5d6570f35109b1e
Kamal <kamalmiftah01@gmail.com>
2024-01-09 20:51:42 +0100

    fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,44 +7,74 @@
  * @idx: the position
  * @n: data of the new node
  * Return: : the address of the new node, or NULL if it failed
-*/
+ */
+
+/**
+ *  * dlistint_len - returns the number of elements in
+ *   * a double linked list
+ *    *
+ *     * @h: head of the list
+ *      * Return: the number of nodes
+ *       */
+unsigned int dlistint_len1(dlistint_t *h)
+{
+	unsigned int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int insert = 1;
+	unsigned int insert = 0;
 	dlistint_t *newnode = NULL;
+	unsigned int len = dlistint_len1(*h);
 
-	if (idx == 0)
-		newnode = add_dnodeint(h, n);
-	else
+	if (len == idx)
 	{
-		newnode = malloc(sizeof(dlistint_t));
-		if (newnode == NULL)
-			return (NULL);
+		newnode = add_dnodeint_end(h, n);
+		return (newnode);
+	}
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
 
-		while ((*h)->prev)
-			*h = (*h)->prev;
-		while (*h)
+	while ((*h)->prev)
+		*h = (*h)->prev;
+
+	while (*h)
+	{
+		if (insert == idx)
 		{
-			if (insert == idx)
+			newnode->n = n;
+			newnode->next = NULL;
+			newnode->prev = NULL;
+			if (*h != NULL)
 			{
-				newnode->n = n;
-				newnode->next = NULL;
-				newnode->prev = NULL;
-				if (*h != NULL)
-				{
-					newnode->prev = (*h)->prev;
-					newnode->next = *h;
-					if (idx != 0)
-						((*h)->prev)->next = newnode;
-					(*h)->prev = newnode;
-				}
-				*h = newnode;
-				return (newnode);
+				newnode->prev = (*h)->prev;
+				newnode->next = *h;
+				if (idx != 0)
+					((*h)->prev)->next = newnode;
+				(*h)->prev = newnode;
 			}
-			*h = (*h)->next;
-			insert++;
+			*h = newnode;
+			return (newnode);
 		}
+		*h = (*h)->next;
+		insert++;
 	}
 	return (NULL);
 }
