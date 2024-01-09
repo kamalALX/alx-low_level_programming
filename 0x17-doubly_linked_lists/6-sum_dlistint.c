9a4898b35b527755ba629d232d756d7f4dbc4b7b
Kamal <kamalmiftah01@gmail.com>
2024-01-09 19:24:11 +0100

    fixing task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,12 +11,15 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (head->prev)
-		head = head->prev;
-	while (head)
+	if (head)
 	{
-		sum += head->n;
-		head = head->next;
+		while (head->prev)
+			head = head->prev;
+		while (head)
+		{
+			sum += head->n;
+			head = head->next;
+		}
 	}
 	return (sum);
 }
