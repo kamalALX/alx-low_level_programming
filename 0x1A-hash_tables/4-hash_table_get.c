5c481a7bbb0162ba5501d73e383d284f761ceb2a
Kamal <kamalmiftah01@gmail.com>
2024-02-02 03:23:16 +0100

    fixing task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,11 +14,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx =  key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node = ht->array[idx];
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	if (idx >= ht->size)
 		return (NULL);
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (NULL);
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
