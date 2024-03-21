ec97cecdfb66a0293243b96582a18d56fd9e868a
kamal <kamalmiftah01@gmail.com>
2024-03-21 00:51:41 +0000

    Solving task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""  """
+""" this module contains method island_perimeter """
 
 
 def island_perimeter(grid):
-
+    """
+        this method eturns the perimeter of the island described in grid
+    """
     rows = len(grid)
     columns = len(grid[0])
     p = 0
@@ -19,4 +21,3 @@ def island_perimeter(grid):
                 if i == rows - 1 or grid[i + 1][j] == 0:
                     p += 1
     return p
-
