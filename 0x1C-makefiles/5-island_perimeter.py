be2efbe4e985a68da52c7ea8fcc97137674f4db2
kamal <kamalmiftah01@gmail.com>
2024-03-21 00:50:04 +0000

    Solving task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""  """
+
+
+def island_perimeter(grid):
+
+    rows = len(grid)
+    columns = len(grid[0])
+    p = 0
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    p += 1
+                if j == columns - 1 or grid[i][j + 1] == 0:
+                    p += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    p += 1
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    p += 1
+    return p
+
