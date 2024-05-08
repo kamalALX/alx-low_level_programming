2596837da5bc4124bea58daf7ca256cae7fe3483
Kamal <kamalmiftah01@gmail.com>
2024-05-08 13:01:37 +0100

    task 11

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,52 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t result = -1;
+	size_t i;
+
+	if (array == NULL || size == 0)
+        return (-1);
+
+    while (left <= right)
+	{
+        int mid = left + (right - left) / 2;
+
+        printf("Searching in array");
+
+        for (i = left; i <= right; i++)
+		{
+            printf("%s %d", (i == left) ? ":" : ",", array[i]);
+        }
+        printf("\n");
+
+        if (array[mid] == value)
+		{
+            result = mid;
+            right = mid - 1;
+        }
+		else if (array[mid] < value)
+		{
+            left = mid + 1;
+        }
+		else
+		{
+            right = mid - 1;
+        }
+    }
+
+    return (result);
+}
